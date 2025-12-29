640f213b184bb290468525f45befc9f836ff54cf
dragsbruh <dragsbruh@protonmail.com>
2025-12-29 20:48:34 +0530

    feat: update server to match new api

--- server.go
+++ server.go
@@ -8,10 +8,11 @@ import (
 	"io"
 	"io/fs"
 	"log"
-	"math/rand/v2"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
@@ -38,15 +39,26 @@ func main() {
 		cacheIntervalStr = "6h"
 	}
 
+	imagesDir, ok := os.LookupEnv("IMAGES_DIR")
+	var imageFs fs.FS = nil
+	if ok {
+		imageFs = os.DirFS(imagesDir)
+	}
+
 	cacheInterval, err := time.ParseDuration(cacheIntervalStr)
 	if err != nil {
 		log.Fatalf("error parsing CACHE_INTERVAL: %v", err)
 	}
 
 	yc := YuCache{
-		Mu:    sync.RWMutex{},
-		Cache: map[string]Yuri{},
-		FS:    os.DirFS(jsonDir),
+		Mu:     sync.RWMutex{},
+		Cache:  map[string]Yuri{},
+		JsonFS: os.DirFS(jsonDir),
+	}
+
+	routes := Routes{
+		YC:      &yc,
+		ImageFS: imageFs,
 	}
 
 	log.Print("seeding yuri")
@@ -56,7 +68,7 @@ func main() {
 
 	server := http.Server{
 		Addr:    addr,
-		Handler: http.StripPrefix("/v1", Router(&yc)),
+		Handler: routes.Router(),
 	}
 
 	wg := sync.WaitGroup{}
@@ -99,7 +111,7 @@ func main() {
 		scancel()
 	}()
 
-	<-ctx.Done()
+	<-sctx.Done()
 }
 
 type Yuri struct {
@@ -118,10 +130,10 @@ type Yuri struct {
 }
 
 type YuCache struct {
-	Mu    sync.RWMutex
-	FS    fs.FS
-	Cache map[string]Yuri
-	Arr   []Yuri
+	Mu     sync.RWMutex
+	JsonFS fs.FS
+	Cache  map[string]Yuri
+	Arr    []Yuri
 }
 
 func (yc *YuCache) Select(n int) []Yuri {
@@ -153,7 +165,7 @@ func (yc *YuCache) Update() error {
 	yc.Mu.Lock()
 	defer yc.Mu.Unlock()
 
-	entries, err := fs.ReadDir(yc.FS, ".")
+	entries, err := fs.ReadDir(yc.JsonFS, ".")
 	if err != nil {
 		return fmt.Errorf("read dir: %w", err)
 	}
@@ -168,18 +180,19 @@ func (yc *YuCache) Update() error {
 			continue
 		}
 
-		f, err := yc.FS.Open(fname)
+		f, err := yc.JsonFS.Open(fname)
 		if err != nil {
 			log.Printf("error opening file: %v", err)
 			continue
 		}
-		defer f.Close()
 
 		var yuri Yuri
 		if err := json.NewDecoder(f).Decode(&yuri); err != nil {
 			log.Printf("json decode error in yuri: %v", err)
+			f.Close()
 			continue
 		}
+		f.Close()
 
 		yc.Cache[fname] = yuri
 	}
@@ -192,37 +205,86 @@ func (yc *YuCache) Update() error {
 	return nil
 }
 
-func Router(yc *YuCache) http.Handler {
+func (c *Routes) Router() http.Handler {
+	api := http.NewServeMux()
+
+	api.HandleFunc("/yuri", c.Yuri)
+
+	api.HandleFunc("/yuri/{cid}/{index}", func(w http.ResponseWriter, r *http.Request) {
+		cid := r.PathValue("cid")
+		indexStr := r.PathValue("index")
+
+		index, err := strconv.ParseInt(indexStr, 10, 64)
+		if err != nil {
+			writeErr(w, http.StatusBadRequest, "invalid number for index")
+			return
+		}
+
+		c.YuriImage(w, cid, index)
+	})
+
+	api.HandleFunc("/yuri/{cid}", func(w http.ResponseWriter, r *http.Request) {
+		c.YuriImage(w, r.PathValue("cid"), 0)
+	})
+
 	r := http.NewServeMux()
 
-	r.HandleFunc("/yuri", func(w http.ResponseWriter, r *http.Request) {
-		countStr := r.URL.Query().Get("n")
-		if countStr == "" {
-			countStr = "10"
-		}
+	r.Handle("/v1/", http.StripPrefix("/v1", api))
 
-		count, err := strconv.ParseInt(countStr, 10, 64)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			writeErr(w, "invalid numeric value for `n`")
-			return
-		}
-
-		if count < 1 || count > 50 {
-			w.WriteHeader(http.StatusBadRequest)
-			writeErr(w, "count must be in range (0, 50]")
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(yc.Select(int(count)))
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "https://github.com/dragsbruh/yuriapi", http.StatusFound)
 	})
 
 	return r
 }
 
-func writeErr(w io.Writer, msg string) error {
+func writeErr(w http.ResponseWriter, status int, msg string) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(map[string]string{
 		"error": msg,
 	})
+}
+
+type Routes struct {
+	YC      *YuCache
+	ImageFS fs.FS
+}
+
+func (c *Routes) Yuri(w http.ResponseWriter, r *http.Request) {
+	countStr := r.URL.Query().Get("n")
+	if countStr == "" {
+		countStr = "10"
+	}
+
+	count, err := strconv.ParseInt(countStr, 10, 64)
+	if err != nil {
+		writeErr(w, http.StatusBadRequest, "invalid numeric value for `n`")
+		return
+	}
+
+	if count < 1 || count > 50 {
+		writeErr(w, http.StatusBadRequest, "count must be in range (0, 50]")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(c.YC.Select(int(count)))
+}
+
+func (c *Routes) YuriImage(w http.ResponseWriter, cid string, index int64) {
+	f, err := c.ImageFS.Open(filepath.Join(cid, fmt.Sprintf("%d.jpeg", index)))
+	if err != nil {
+		if os.IsNotExist(err) {
+			writeErr(w, http.StatusNotFound, "no such yuri")
+		} else {
+			log.Printf("error opening image %s/%d: %v", cid, index, err)
+			writeErr(w, http.StatusInternalServerError, "couldnt read image")
+		}
+		return
+	}
+	defer f.Close()
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	io.Copy(w, f)
 }
